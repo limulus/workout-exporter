1c4fc2150a7d95a423f6d37611f0189915a3fcbe
Eric McCarthy <eric@limulus.net>
2025-02-15 14:02:01 -0700

    Initial Commit

---
+++ Workout Exporter/Workout_ExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_ExporterApp.swift
+//  Workout Exporter
+//
+//  Created by Eric McCarthy on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_ExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Workout ExporterTests/Workout_ExporterTests.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_ExporterTests.swift
+//  Workout ExporterTests
+//
+//  Created by Eric McCarthy on 2/15/25.
+//
+
+import Testing
+@testable import Workout_Exporter
+
+struct Workout_ExporterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
